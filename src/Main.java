4994875b327ebe6c0d71ca4a433f9e961fd19132
VictorGotsenko <victor.gotsenko@gmail.com>
2023-12-13 17:52:37 +0500

    readWordsFromFile - done

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,34 @@
 import java.io.*;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * Created by Me on 12.12.2023.
  */
 public class Main {
     public static void main(String[] args) {
-        // readWordsfromFile
+       readWordsFromFile();
+
+
+
         // offerGame
         // gamingLoop
         System.out.println("Hello!");
     }
 
     public static void readWordsFromFile () {
-        String sPath;
+        ArrayList<String> wordsDictionary4game = new ArrayList<>();
         try {
-            File file = new File(sPath);
+            File file = new File(String.valueOf(Paths.get("src", "WordsDictionary.txt").toFile()));
             FileReader fr = new FileReader(file);
             BufferedReader reader = new BufferedReader(fr);
             // считаем сначала первую строку
             String line = reader.readLine();
             while (line != null) {
+                wordsDictionary4game.add(line);
                 System.out.println(line);
                 // считываем остальные строки в цикле
-                line = reader.readLine();
+                line= reader.readLine();
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
