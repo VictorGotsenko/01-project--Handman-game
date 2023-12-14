cb5b7129454d970bc3ec608dfe2acede31c4d1b3
VictorGotsenko <victor.gotsenko@gmail.com>
2023-12-14 17:03:01 +0500

    printGallow - done

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> wordsDictionary4game = new ArrayList<>();
+
         Set<Character> chArrayWrongChars = new LinkedHashSet<>();       // массив для неправ.введённых букв
       //  inviteGame();
    //     System.out.println("Hello!");
 
         wordsDictionary4game= readWordsFromFile();
-       String strGuessWord= guessTheWord(wordsDictionary4game);
+        String strGuessWord= guessTheWord(wordsDictionary4game);        // Загаданное слово...
         System.out.println(strGuessWord);
+        printGallow(7);
 
 
 
@@ -94,10 +96,7 @@ public class Main {
         }
     }
     public static String guessTheWord(ArrayList<String> wordsDictionary4game) {
-       /*
-        List<Character> chArrayGuessTheWord = new ArrayList<>();        // массив для загаданного слова
-        List<Character> chArrayRresult = new ArrayList<>();             // массив для отгаданных букв
-       */
+
         Random numRnd = new Random();
         int iNumRnd = numRnd.nextInt(wordsDictionary4game.size());      // random int
         String sGuessTheWord = wordsDictionary4game.get(iNumRnd);
@@ -114,6 +113,115 @@ public class Main {
         return sGuessTheWord;
     }
 
+    public static boolean gamePlay(String stGuessWord) {
+        List<Character> chArrayGuessTheWord = new ArrayList<>();        // массив для загаданного слова
+        List<Character> chArrayRresult = new ArrayList<>();             // массив для отгаданных букв
+        boolean isWin = false;
+        char[] cGuessTheWord = stGuessWord.toCharArray();
+        for (char c : cGuessTheWord) {                                       // слово в коллекцию букв
+            chArrayGuessTheWord.add(c);
+            chArrayRresult.add('_');                                         // коллекцию для отгаданных букв заполнить _ подчёркиванием
+        }
+        System.out.println("Загаданное слово...");
+        for (int i = 0; i < chArrayRresult.size(); i++) {                    // вывод коллекции _ _ _ _
+            System.out.print(chArrayRresult.get(i) + " ");
+        }
+        System.out.println();
+        /*******************
+         Игровой цикл
+         *******************/
+       /* Gallow gGame1 = new Gallow();                                       // инициализация виселицы
+        gGame1.GallowPrint();                                               // печать виселицы
+        int iError = 0;
+        boolean isCharHere;
+        boolean isWin = false;
+        while (iError < 6) {                                                // играем до 6 ошибок
+            isCharHere = false;
+            do {
+                System.out.print("Введите одну русскую букву: ");
+                s = cReadFromKeyboard.nextLine();
+                s = s.toLowerCase();
+            }
+            while (!s.matches("[а-я]"));                      // Выполнить проверку на принадлежность диапазона ru букв
+            cKey = s.charAt(0);
+            System.out.println();
+            System.out.println("Загаданное слово...");
+            for (int i = 0; i < chArrayGuessTheWord.size(); i++) {          // сравнение посимвольно загаданного слова
+                if (cKey == chArrayGuessTheWord.get(i)) {
+                    chArrayRresult.set(i, cKey);                            //поместить букву в массив угаданных
+                    isCharHere = true;
+                }
+                System.out.print(chArrayRresult.get(i) + " ");              // напечатать угаданные буквы
+            }*/
+
+
+
+
+
+
+
+
+
+
+
+            return isWin;
+    }
+
+
+    public static void printGallow(int iError) {
+        String sGallow1 = " ____";
+        String sGallow2 = " |  |";
+//    String sGallow3 = " |  o";
+//    String sGallow4 = " | /|\\";
+//    String sGallow5 = " | /`"+"\\";
+        String sGallow3 = " |  ";
+        String sGallow4 = " | ";
+        String sGallow5 = " | ";
+        String sGallow6 = " | ";
+        String sGallow7 = "/|"+"\\";
+
+        ///
+        if (iError > 6) {
+            System.out.println("Количество ошибок равно "+iError+" печать виселицы невозможна!");
+            return;
+        }
+        switch (iError) {
+            case 1:
+                sGallow3 = " |  o";
+                break;
+            case 2:
+                sGallow4 = " |  |";
+                break;
+            case 3:
+                sGallow4 = " | /|";
+                break;
+            case 4:
+                sGallow4 = " | /|\\";
+                break;
+            case 5:
+                sGallow5 = " | /`";
+                break;
+            case 6:
+                sGallow5 = " | /`\\";
+                break;
+            default:
+                break;
+        }
+
+        System.out.println(sGallow1);
+        System.out.println(sGallow2);
+        System.out.println(sGallow3);
+        System.out.println(sGallow4);
+        System.out.println(sGallow5);
+        System.out.println(sGallow6);
+        System.out.println(sGallow7);
+        System.out.println();
+
+
+
+
+
+    }
 
 
 }   //end Main
