5f9618e262bb94a1d2b8623a43d26f59b8a7ef3c
VictorGotsenko <victor.gotsenko@gmail.com>
2023-12-15 18:21:45 +0500

    program finish

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,26 @@ import java.util.*;
 
 /**
  * -= java-backend-learning-course =- (с) Sergey Zhukov
- *https://zhukovsd.github.io/java-backend-learning-course/
- *
+ * https://zhukovsd.github.io/java-backend-learning-course/
+ * <p>
  * Project 01 game Handman
  * written in procedural style
- *
+ * <p>
  * Проект 01  игра Виселица
  * написан в процедурном стиле
- *
+ * <p>
  * Created by VictorGotsenko on 12.12.2023.
  */
 public class Main {
     private static Scanner cReadFromKeyboard = new Scanner(System.in);
+
     public static void main(String[] args) {
-        ArrayList<String> wordsDictionary4game = new ArrayList<>(); // collection of  words
+        ArrayList<String> wordsDictionary4game = new ArrayList<>();                                                     // collection of  words
         boolean isWin = false;
         System.out.println(" Добро пожаловать в игру Виселица");
         while (inviteGame()) {
             wordsDictionary4game = readWordsFromFile();
-            String strGuessWord = guessTheWord(wordsDictionary4game);        // Загаданное слово...
+            String strGuessWord = guessTheWord(wordsDictionary4game);                                                   // Загаданное слово...
             isWin = gamePlay(strGuessWord);
             if (isWin) {
                 System.out.println("Это победа ! Вы выйграли !!!");
@@ -32,53 +33,54 @@ public class Main {
             }
             System.out.println("      -= Игра окончена =- ");
             System.out.println(" -= Но можно сыграть снова =- ");
-            }
         }
+    }
 
-    public static ArrayList<String> readWordsFromFile () {
-        int iCountLines = 0;                                                // инициализация переменных
-        int iLenghtWord = 5;
+    public static ArrayList<String> readWordsFromFile() {
+        int iLenghtWordMax = 10;                                                                                        // максимальная длинна слова
+        int iLenghtWordMin = 5;                                                                                         // минимальная
         String sWorkDir = "src";
         String sWordsDictionary = "WordsDictionary.txt";
         char chTmp;
-        String sTmpLine="а"; // temp var
+        String sTmpString = "а";                                                                                         // temp var
         ArrayList<String> wordsDictionary4game = new ArrayList<>();
         try {
             File file = new File(String.valueOf(Paths.get(sWorkDir, sWordsDictionary).toFile()));
             FileReader fr = new FileReader(file);
             BufferedReader reader = new BufferedReader(fr);
- labelOne:  while ((sTmpLine = reader.readLine()) != null) {           // считываем строки в цикле
-                sTmpLine= sTmpLine.toLowerCase();
-                if ( sTmpLine.length() < iLenghtWord ) continue;     // обработка слова (менее 5 букв отбросить)
-
-                for (int i = 0; i < sTmpLine.length(); i++) {       // проверка - слово содержит только русские буквы ?
-                    chTmp = sTmpLine.charAt(i);
+            labelOne:
+            while ((sTmpString = reader.readLine()) != null) {                                                          // считываем строки в цикле
+                sTmpString = sTmpString.toLowerCase();
+                if ((sTmpString.length() < iLenghtWordMin) || (sTmpString.length() > iLenghtWordMax))
+                    continue;                                                                                           // обработка слова (менее 5 или более 10  букв отбросить)
+                for (int i = 0; i < sTmpString.length(); i++) {                                                         // проверка - слово содержит только русские буквы ?
+                    chTmp = sTmpString.charAt(i);
                     if (!(chTmp >= 'а' && chTmp <= 'я')) {
-                        if (chTmp == 'ё') { continue; }
-                        else {                                       //слово с символами не входящими в русский алфавит отбрасывать
-                            System.out.println("Слово: " + sTmpLine + " содержит недопустимые символы и было отброшено ");
+                        if (chTmp == 'ё') {
+                            continue;
+                        } else {                                                                                        //слово с символами не входящими в русский алфавит отбрасывать
+                            System.out.println("Слово: " + sTmpString + " содержит недопустимые символы и было отброшено ");
                             continue labelOne;
                         }
                     }
                 }
-                wordsDictionary4game.add(sTmpLine);
-            //    System.out.println(sTmpLine);
+                wordsDictionary4game.add(sTmpString);
             }
-        reader.close();
-        fr.close();
+            reader.close();
+            fr.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // обработка файла с малым количеством слов < 5
-        if (wordsDictionary4game.size() <= 5) {                     // обработка файла с малым количеством слов < 5
+        if (wordsDictionary4game.size() <= 5) {                                                                         // обработка файла с малым количеством слов < 5
             System.out.println("Файла словаря" + sWordsDictionary + " было загружено менее 6 слов. Для работы программы необходимо больше слов");
             System.out.println("Работа программы завершена");
             System.exit(0);
         }
         return wordsDictionary4game;
     }
+
     public static boolean inviteGame() {
         boolean isGaming = false;
         String strngTemp;
@@ -104,38 +106,29 @@ public class Main {
         }
         return isGaming;
     }
+
     public static String guessTheWord(ArrayList<String> wordsDictionary4game) {
 
         Random numRnd = new Random();
-        int iNumRnd = numRnd.nextInt(wordsDictionary4game.size());      // random int
+        int iNumRnd = numRnd.nextInt(wordsDictionary4game.size());                                                      // random int
         String sGuessTheWord = wordsDictionary4game.get(iNumRnd);
-
-       /*// chArrayGuessTheWord = sTmp.toCharArray();
-       // String sGuessTheWord = GuessTheWord.RndWord(iNumLine);                // Загадать слово
-        char[] cGuessTheWord = sGuessTheWord.toCharArray();
-        for (char c : cGuessTheWord) {                                       // слово в коллекцию букв
-            chArrayGuessTheWord.add(c);
-            chArrayRresult.add('_');                                         // коллекцию для отгаданных букв заполнить _ подчёркиванием
-        }
-       // return chArrayGuessTheWord, chArrayRresult;
-*/
         return sGuessTheWord;
     }
 
     public static boolean gamePlay(String stGuessWord) {
-        List<Character> chArrayGuessTheWord = new ArrayList<>();        // массив для загаданного слова
-        List<Character> chArrayRresult = new ArrayList<>();             // массив для отгаданных букв
-        Set<Character> chArrayWrongChars = new LinkedHashSet<>();       // массив для неправ.введённых букв
-        char chMask = '_';                                              // маска закрывающая буквы
+        List<Character> chArrayGuessTheWord = new ArrayList<>();                                                        // массив для загаданного слова
+        List<Character> chArrayRresult = new ArrayList<>();                                                             // массив для отгаданных букв
+        Set<Character> chArrayWrongChars = new LinkedHashSet<>();                                                       // массив для неправ.введённых букв
+        char chMask = '_';                                                                                              // маска закрывающая буквы
         int iError = 0;
         boolean isCharHere;
         boolean isWin = false;
-        for (int i = 0; i <stGuessWord.length() ; i++) {                // слово в коллекцию букв
+        for (int i = 0; i < stGuessWord.length(); i++) {                                                                // слово в коллекцию букв
             chArrayGuessTheWord.add(stGuessWord.charAt(i));
-            chArrayRresult.add(chMask);                                 // коллекцию для отгаданных букв заполнить маской
+            chArrayRresult.add(chMask);                                                                                 // коллекцию для отгаданных букв заполнить маской
         }
         System.out.println("Загаданное слово...");
-        for (int i = 0; i < chArrayRresult.size(); i++) {                    // вывод коллекции _ _ _ _
+        for (int i = 0; i < chArrayRresult.size(); i++) {                                                               // вывод коллекции _ _ _ _
             System.out.print(chArrayRresult.get(i) + " ");
         }
         System.out.println();
@@ -144,18 +137,18 @@ public class Main {
          *******************/
         printHangman(iError); // печать виселицы
         char cKey;
-        while (iError < 6) {                                                // играем до 6 ошибок
+        while (iError < 6) {                                                                                             // играем до 6 ошибок
             isCharHere = false;
-            cKey= getChar();
+            cKey = getChar();
             System.out.println();
 
             System.out.println("Загаданное слово...");
-            for (int i = 0; i < chArrayGuessTheWord.size(); i++) {          // сравнение посимвольно загаданного слова
+            for (int i = 0; i < chArrayGuessTheWord.size(); i++) {                                                      // сравнение посимвольно загаданного слова
                 if (cKey == chArrayGuessTheWord.get(i)) {
-                    chArrayRresult.set(i, cKey);                            //поместить букву в массив угаданных
+                    chArrayRresult.set(i, cKey);                                                                        //поместить букву в массив угаданных
                     isCharHere = true;
                 }
-                System.out.print(chArrayRresult.get(i) + " ");              // напечатать угаданные буквы
+                System.out.print(chArrayRresult.get(i) + " ");                                                          // напечатать угаданные буквы
             }
             System.out.println();
             /** варианты:  1 буква в слове есть - поместить в массив угаданных
@@ -164,7 +157,7 @@ public class Main {
              */
             if (isCharHere == false) {
                 if (chArrayWrongChars.add(cKey)) {
-                    iError++;                                              // буквы нет, плюсую ошибку
+                    iError++;                                                                                           // буквы нет, плюсую ошибку
                 } else {
                     System.out.println("\nТакую букву вы уже вводили и ее нет в слове");
                 }
@@ -172,15 +165,15 @@ public class Main {
             System.out.println("Ошибки (" + iError + "):" + chArrayWrongChars);
             System.out.println("");
             printHangman(iError);
-            if (!chArrayRresult.contains(chMask)) {                   // слово отгаданно, если нет символов маски
+            if (!chArrayRresult.contains(chMask)) {                                                                     // слово отгаданно, если нет символов маски
                 isWin = true;
                 break;
             }
         } // end While
-        chArrayGuessTheWord.removeAll( chArrayGuessTheWord );  //****         очистка коллекций после игрового раунда
+        chArrayGuessTheWord.removeAll(chArrayGuessTheWord);                                                             //****         очистка коллекций после игрового раунда
         chArrayRresult.removeAll(chArrayRresult);
         chArrayWrongChars.removeAll(chArrayWrongChars);
-        System.gc();                                            // Уборка мусора
+        System.gc();                                                                                                    // Уборка мусора
         return isWin;
     }
 
@@ -194,11 +187,9 @@ public class Main {
         String sGallow4 = " | ";
         String sGallow5 = " | ";
         String sGallow6 = " | ";
-        String sGallow7 = "/|"+"\\";
-
-        ///
+        String sGallow7 = "/|" + "\\";
         if (iError > 6) {
-            System.out.println("Количество ошибок равно "+iError+" печать виселицы невозможна!");
+            System.out.println("Количество ошибок равно " + iError + " печать виселицы невозможна!");
             return;
         }
         switch (iError) {
@@ -230,7 +221,6 @@ public class Main {
             default:
                 break;
         }
-
         System.out.println(sGallow1);
         System.out.println(sGallow2);
         System.out.println(sGallow3);
@@ -249,13 +239,8 @@ public class Main {
             strTmp = cReadFromKeyboard.nextLine();
             strTmp = strTmp.toLowerCase();
         }
-        while (!strTmp.matches("[а-я]"));                      // Выполнить проверку на принадлежность диапазона ru букв
+        while (!strTmp.matches("[а-я]"));                                                                               // Выполнить проверку на принадлежность диапазона ru букв
         cKey = strTmp.charAt(0);
         return cKey;
     }
-
-
-
-
-
 }   //end Main
